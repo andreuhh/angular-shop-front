965a6885cbeae89369fa7ea46230144905df8560
Andrea Guffi <andreaguffi92@gmail.com>
2022-10-09 23:22:05 +0200

    first commit

---
+++ apps/admin/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './pagas/dashboard/dashboard.component';
+
+@NgModule({
+    declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
+    imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { UiModule } from '@bluebits/ui';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: HomePageComponent
+    },
+    {
+        path: 'products',
+        component: ProductListComponent
+    }
+];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent,],
+    imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
+    providers: [],
+    bootstrap: [AppComponent],
+    exports: [
+
+    ]
 })
-export class AppModule {}
+export class AppModule { }
