783f71eae7a7ec085efb0a4b6bd9a1822cea58ea
Andrea Guffi <andreaguffi92@gmail.com>
2022-10-10 22:45:18 +0200

    ok

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { ProductListComponent } from './pages/product-list/product-list.componen
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { UiModule } from '@bluebits/ui';
+import { AccordionModule } from 'primeng/accordion';
 
 const routes: Routes = [
     {
@@ -22,7 +24,7 @@ const routes: Routes = [
 
 @NgModule({
     declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent,],
-    imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
+    imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes), UiModule, AccordionModule,],
     providers: [],
     bootstrap: [AppComponent],
     exports: [
