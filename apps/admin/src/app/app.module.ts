b205d00035b1ad15441f1c0bcbebaafa63dc930a
Andrea Guffi <andreaguffi92@gmail.com>
2022-10-13 09:28:01 +0200

    get categories

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -11,6 +12,7 @@ import { CardModule } from 'primeng/card';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { CategoriesService } from '@bluebits/products';
 
 const UX_MODULE = [
     CardModule,
@@ -40,10 +42,11 @@ const routes: Routes = [
     declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent],
     imports: [
         BrowserModule,
+        HttpClientModule,
         RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
         ...UX_MODULE
     ],
-    providers: [],
+    providers: [CategoriesService],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
--- apps/admin/src/app/categories/categories-list/categories-list.component.ts
+++ apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService, Category } from '@bluebits/products';
 
 @Component({
     selector: 'bluebits-categories-list',
@@ -7,25 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesListComponent implements OnInit {
 
-    categories = [
-        {
-            id: 1,
-            name: "category-1",
-            icon: "icon-1"
-        },
-        {
-            id: 2,
-            name: "category-1",
-            icon: "icon-1"
-        },
-        {
-            id: 3,
-            name: "category-1",
-            icon: "icon-1"
-        }
-    ]
+    // categories = [
+    //     {
+    //         id: 1,
+    //         name: "category-1",
+    //         icon: "icon-1"
+    //     },
+    //     {
+    //         id: 2,
+    //         name: "category-1",
+    //         icon: "icon-1"
+    //     },
+    //     {
+    //         id: 3,
+    //         name: "category-1",
+    //         icon: "icon-1"
+    //     }
+    // ]
 
-    constructor() { }
+    categories: Category[] = [];
 
-    ngOnInit(): void { }
+    constructor(private categoriesService: CategoriesService) { }
+
+    ngOnInit(): void {
+        this.categoriesService.getCategories().subscribe(
+            (cats) => {
+                this.categories = cats;
+            }
+        )
+    }
 }
